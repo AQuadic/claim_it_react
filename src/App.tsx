d62d2cd8cc8fd63fc3eb0a4bd68ea7374e68e8e4
ahmed <ahmedabozaid.dev@gmail.com>
2025-05-29 13:28:23 +0300

    changes routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import Layout from "./components/general/Layout";
 import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
+          <Route index element={<Navigate to="/not_found" replace />} />
+          <Route path="/coupons/:id" element={<HomePage />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
