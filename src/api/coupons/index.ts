9ae5a7e479165c2e56a1a6926b964f1b77df0c3f
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-29 12:01:32 +0300

    api

---
+++ src/api/coupons/index.ts
@@ -0,0 +1,8 @@
+import axios  from "@/lib/axios";
+
+export const hashCoupon = async (hash: string) => {
+    const response = await axios.get("/coupons", {
+        params: { hash },
+    });
+    return response.data;
+};
